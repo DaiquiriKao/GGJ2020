9d7b0ff1f57dad32cea5fe794a4e0776c08c2cb8
DaiquiriKao <47049570+DaiquiriKao@users.noreply.github.com>
2020-02-02 14:57:19 +0800

    製作首頁

---
+++ Demo_Repair/Assets/Script/TopPageMain.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TopPageMain : MonoBehaviour
+{
+    public Animator anim;
+    public Button btn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn.onClick.AddListener(delegate { StartCoroutine(PlayAnim()); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator PlayAnim()
+    {
+        anim.SetTrigger("Start");
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("Game");
+    }
+}
